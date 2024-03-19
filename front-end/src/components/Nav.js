5d47e656ce7ef570f42b7f7674b63839d3b07acc
deep-coder1 <ndktbd@gmail.com>
2024-03-19 07:44:42 +0530

    Make a function for logout

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Nav=()=>{
     const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () =>{
+        // console.warn("Logout")
+        localStorage.clear();
+        navigate('/signup');
+    }
     return(
         <div>
             <ul className='nav-ul'>
@@ -10,7 +16,7 @@ const Nav=()=>{
                 <li><Link to="/add">Add Product</Link></li>
                 <li><Link to="/update">Update Product</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{ auth ? <Link to="/logout">Logout</Link> : 
+                <li>{ auth ? <Link onClick={logout} to="/signup">Logout</Link> : 
                 <Link to="/signup">Sign Up</Link>}</li>
             </ul>
         </div>
