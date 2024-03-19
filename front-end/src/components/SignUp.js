63b2e759a8f0e6ed1ab71135271c9ab7f08e6458
deep-coder1 <ndktbd@gmail.com>
2024-03-19 06:59:16 +0530

    Complete SignUp Flow

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {useNavigate} from 'react-router-dom'
 
 const SignUp = () => {
@@ -6,6 +6,14 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  useEffect(()=>{
+      const auth = localStorage.getItem('user');
+      if(auth)
+      {
+          navigate('/')
+      }
+  })
 
   const collectData = async () => {
     console.warn(name, email, password);
@@ -19,6 +27,7 @@ const SignUp = () => {
     // console.warn(await result.json);
     result = await result.json();
     console.warn(result);
+    localStorage.setItem("user",JSON.stringify(result));
     if(result)
     {
       navigate('/');
