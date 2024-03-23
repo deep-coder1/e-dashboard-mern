0945d22ff2c417528fc27160f018916c5156eb62
deep-coder1 <ndktbd@gmail.com>
2024-03-23 06:28:46 +0530

    React js Navbar Update

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -11,20 +11,25 @@ const Nav=()=>{
     }
     return(
         <div>
-            <ul className='nav-ul'>
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/add">Add Product</Link></li>
-                <li><Link to="/update">Update Product</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                {
-                    auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    :
-                    <>
+            <img 
+            src='https://yt3.ggpht.com/ytc/AKedOLR09bCpy_XTq2scU91URc0pWG0EqS_Yc_Zg-r9pBQ=s900-c-k-c0x00ffffff-no-rj' 
+            alt='logo' 
+            className='logo'
+            />
+            { auth ?
+                <ul className='nav-ul'>
+                    <li><Link to="/">Products</Link></li>
+                    <li><Link to="/add">Add Product</Link></li>
+                    <li><Link to="/update">Update Product</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
+                </ul>
+                : 
+                <ul className='nav-ul nav-right'>
                     <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    </>
-                }
-            </ul>
+                        <li><Link to="/login">Login</Link></li>
+                </ul>
+            }
         </div>
     )
 }
